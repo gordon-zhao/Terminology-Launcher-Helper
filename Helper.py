f11bf058dc98d705d514921b7105fd10e2d90280
gordon-zhao <zhaoyiteng@hotmail.com>
2016-10-26 15:45:53 -0400

    Another !

--- Helper.py
+++ Helper.py
@@ -718,7 +718,7 @@ def create_package():
         else:
             continue
     if not found_version: 
-        print 'Your option is wrong! Please restart the program！'
+        print 'Your option is wrong! Please restart the program! '
         return -1
     dict_a=Generator_libraries_search(Dot_Minecraft_Path,select,dict_a)
     file_and_tuple=Generator_calculate_file_to_zip(Dot_Minecraft_Path,dict_a,select)
